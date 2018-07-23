0440a08de6406e93a63e986d974dec23ab44362b
juanhaces <juan.m.hacescrespo@tu-berlin.de>
2018-07-23 12:50:18 +0200

    Update for Docker from source

--- dockerfiles/install_ros2_source.sh
+++ dockerfiles/install_ros2_source.sh
@@ -1,15 +1,17 @@
 #!/bin/bash 
 
+# It is supposed to work without setting the variable. Try both!
 ROS_DISTRO=ardent
 
-#apt update && apt install -y curl
-apt-get install -y lsb-release curl
+apt update && apt install curl
+#apt-get install -y lsb-release curl
 curl http://repo.ros2.org/repos.key | apt-key add -
 
 sh -c 'echo "deb [arch=amd64,arm64] http://repo.ros2.org/ubuntu/main `lsb_release -cs` main" > /etc/apt/sources.list.d/ros2-latest.list'
 #sh -c 'echo "deb [arch=amd64,arm64] http://repo.ros2.org/ubuntu/main xenial main" > /etc/apt/sources.list.d/ros2-latest.list'
 
-apt-get update
+#apt-get update
+apt update
 
 #apt-get install -y `apt list "ros-$ROS_DISTRO-*" 2> /dev/null | \
 #  grep '/' | awk -F/ '{print $1}' | \
@@ -24,7 +26,7 @@ apt install -y `apt list "ros-$ROS_DISTRO-*" 2> /dev/null | \
   grep '/' | awk -F/ '{print $1}' | \
   grep -v -e ros-$ROS_DISTRO-ros1-bridge \
   -e ros-$ROS_DISTRO-turtlebot2- \
-  -e "ros-$ROS_DISTRO.*-dbgsym"`
+  -e "ros-$ROS_DISTRO.*-dbgsym" `
   
   
 #echo `apt list "ros-$ROS_DISTRO-*" 2> /dev/null | \
