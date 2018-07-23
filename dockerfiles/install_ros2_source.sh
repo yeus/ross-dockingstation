7593a5cf1051b682b31caba952706e7fccd881e8
thomas meschede / yeus <thomas.meschede@tu-berlin.de>
2018-07-23 14:43:27 +0200

    build ros from source

--- dockerfiles/install_ros2_source.sh
+++ dockerfiles/install_ros2_source.sh
@@ -1,36 +1,13 @@
 #!/bin/bash 
 
-# It is supposed to work without setting the variable. Try both!
-ROS_DISTRO=ardent
+mkdir -p ~/ros2_ws/src
+cd ~/ros2_ws
+#wget https://raw.githubusercontent.com/ros2/ros2/release-latest/ros2.repos
+vcs import src < ros2.repos
 
-apt update && apt install curl
-#apt-get install -y lsb-release curl
-curl http://repo.ros2.org/repos.key | apt-key add -
+rosdep init
+rosdep update
+rosdep install --from-paths src --ignore-src --rosdistro bouncy -y --skip-keys "console_bridge fastcdr fastrtps libopensplice67 rti-connext-dds-5.3.1 urdfdom_headers"
 
-sh -c 'echo "deb [arch=amd64,arm64] http://repo.ros2.org/ubuntu/main `lsb_release -cs` main" > /etc/apt/sources.list.d/ros2-latest.list'
-#sh -c 'echo "deb [arch=amd64,arm64] http://repo.ros2.org/ubuntu/main xenial main" > /etc/apt/sources.list.d/ros2-latest.list'
-
-#apt-get update
-apt update
-
-#apt-get install -y `apt list "ros-$ROS_DISTRO-*" 2> /dev/null | \
-#  grep '/' | awk -F/ '{print $1}' | \
-#  grep -v -e ros-$ROS_DISTRO-ros1-bridge \
-#  -e ros-$ROS_DISTRO-turtlebot2- \
-#  -e "ros-$ROS_DISTRO.*-dbgsym"
-#  -e "ros-$ROS_DISTRO-.*opensplice" \
-#  -e "ros-$ROS_DISTRO-.*connext" | tr '\n' ' '`
-
-#has to be apt, because only apt has the "list" command
-apt install -y `apt list "ros-$ROS_DISTRO-*" 2> /dev/null | \
-  grep '/' | awk -F/ '{print $1}' | \
-  grep -v -e ros-$ROS_DISTRO-ros1-bridge \
-  -e ros-$ROS_DISTRO-turtlebot2- \
-  -e "ros-$ROS_DISTRO.*-dbgsym" `
-  
-  
-#echo `apt list "ros-$ROS_DISTRO-*" 2> /dev/null | \
-#  grep '/' | awk -F/ '{print $1}' | \
-#  grep -v -e ros-$ROS_DISTRO-ros1-bridge \
-#  -e ros-$ROS_DISTRO-turtlebot2- \
-#  -e "ros-$ROS_DISTRO.*-dbgsym"`
+cd ~/ros2_ws/
+colcon build --symlink-install
