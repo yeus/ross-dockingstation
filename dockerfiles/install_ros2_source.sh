1be1593fefa703b0c991168c898eae61511bdbf3
juanhaces <juan.m.hacescrespo@tu-berlin.de>
2018-07-24 10:24:09 +0200

    Ros from source (cmake and extra packages)

--- dockerfiles/install_ros2_source.sh
+++ dockerfiles/install_ros2_source.sh
@@ -3,6 +3,7 @@
 mkdir -p ~/ros2_ws/src
 cd ~/ros2_ws
 #wget https://raw.githubusercontent.com/ros2/ros2/release-latest/ros2.repos
+mv /tmp/ros2.repos ~/ros2_ws
 vcs import src < ros2.repos
 
 rosdep init
