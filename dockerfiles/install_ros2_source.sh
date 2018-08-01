7332831d897b0b5b1a8395e8b935eb0a585d1a54
thomas meschede / yeus <thomas.meschede@tu-berlin.de>
2018-08-01 14:18:49 +0200

    tmp commit

--- dockerfiles/install_ros2_source.sh
+++ dockerfiles/install_ros2_source.sh
@@ -1,4 +1,5 @@
 #!/bin/bash 
+#https://github.com/ros2/ros2/wiki/Linux-Development-Setup
 
 mkdir -p ~/ros2_ws/src
 cd ~/ros2_ws
@@ -6,9 +7,12 @@ cd ~/ros2_ws
 mv /tmp/ros2.repos ~/ros2_ws
 vcs import src < ros2.repos
 
+#apt install -q -y rti-connext-dds-5.3.1  # from repo.ros2.org
+#cd /opt/rti.com/rti_connext_dds-5.3.1/resource/scripts && source ./rtisetenv_x64Linux3gcc5.4.0.bash; cd -
 
 rosdep init
 rosdep update
+#rosdep install --from-paths src --ignore-src --rosdistro bouncy -y --skip-keys "console_bridge fastcdr fastrtps libopensplice67 rti-connext-dds-5.3.1 urdfdom_headers"
 rosdep install --from-paths src --ignore-src --rosdistro bouncy -y --skip-keys "console_bridge fastcdr fastrtps libopensplice67 rti-connext-dds-5.3.1 urdfdom_headers"
 
 
