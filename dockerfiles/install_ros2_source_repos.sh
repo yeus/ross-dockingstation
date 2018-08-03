2f1b05a3bdd200a3728152eeebfe4d8ced431dde
thomas meschede / yeus <thomas.meschede@tu-berlin.de>
2018-08-03 14:40:19 +0200

    reorganization of dockerfiles (again ^^)

--- dockerfiles/install_ros2_source_repos.sh
+++ dockerfiles/install_ros2_source_repos.sh
@@ -3,14 +3,17 @@
 mkdir -p ~/ros2_ws/src
 cd ~/ros2_ws
 #wget https://raw.githubusercontent.com/ros2/ros2/release-latest/ros2.repos
-mv /tmp/ros2.repos ~/ros2_ws
-vcs import src < ros2.repos
+mv /tmp/ros2_iboss.repos ~/ros2_ws
+vcs import src < ros2_iboss.repos
 
 
 rosdep init
 rosdep update
+#rosdep install --from-paths src --ignore-src --rosdistro bouncy -y --skip-keys "console_bridge fastcdr fastrtps libopensplice67 rti-connext-dds-5.3.1 urdfdom_headers"
 rosdep install --from-paths src --ignore-src --rosdistro bouncy -y --skip-keys "console_bridge fastcdr fastrtps libopensplice67 rti-connext-dds-5.3.1 urdfdom_headers"
 
 
+
 cd ~/ros2_ws/
-colcon build --cmake-args -DSECURITY=ON --no-warn-unused-cli --symlink-install
+#ament build --cmake-args -DSECURITY=ON --no-warn-unused-cli --symlink-install
+colcon build --symlink-install
