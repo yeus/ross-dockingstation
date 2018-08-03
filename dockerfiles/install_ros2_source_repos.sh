5125c92df3c15fd0ccf3494e2c56daa97f3b1aa3
juanhaces <juan.m.hacescrespo@tu-berlin.de>
2018-08-03 11:55:08 +0200

    Attempt with bionic and repositories from Frank

---
+++ dockerfiles/install_ros2_source_repos.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+
+mkdir -p ~/ros2_ws/src
+cd ~/ros2_ws
+#wget https://raw.githubusercontent.com/ros2/ros2/release-latest/ros2.repos
+mv /tmp/ros2.repos ~/ros2_ws
+vcs import src < ros2.repos
+
+
+rosdep init
+rosdep update
+rosdep install --from-paths src --ignore-src --rosdistro bouncy -y --skip-keys "console_bridge fastcdr fastrtps libopensplice67 rti-connext-dds-5.3.1 urdfdom_headers"
+
+
+cd ~/ros2_ws/
+colcon build --cmake-args -DSECURITY=ON --no-warn-unused-cli --symlink-install
--- dockerfiles/ros2_source_entrypoint.sh
+++ dockerfiles/ros2_source_entrypoint.sh
@@ -2,6 +2,6 @@
 set -e
 
 # setup ros2 environment
-source $ROS2_WS/setup.bash     
-source $ROS2_WS/install/ros2cli/share/ros2cli/environment/ros2-argcomplete.bash     
+source "$ROS2_WS/install/local_setup.bash"
+# This allows to run any command
 exec "$@"
